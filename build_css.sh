f4f30973d4795833ee2c84f043bf82c6959e6041
NObodyGX <nobodygx@163.com>
2024-07-08 16:23:18 +0800

    update css

--- build_css.sh
+++ build_css.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-npx tailwindcss -i ./css/main.css -o ./static/css/norwind.css --minify
+
+if [ $# == 0 ];then
+    npx tailwindcss -i ./css/main.css -o ./static/css/norwind.css --minify
+else
+    npx tailwindcss -i ./css/main.css -o ./static/css/norwind.css --watch
+fi
